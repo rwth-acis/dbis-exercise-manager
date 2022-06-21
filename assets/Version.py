085a920ab79b8cc79c9a9a7fa5af2d6de17e4684
Philipp Hochmann <phil.hochmann@gmail.com>
2022-06-21 18:34:12 +0200

    Add import os

--- assets/Version.py
+++ assets/Version.py
@@ -1,4 +1,4 @@
 class Version(object):
     name="excmanager"
     description="RWTH Aachen Computer Science i5/dbis assets for Lecture Datenbanken und Informationssysteme"
-    version='0.1.7'
+    version='0.1.8'
