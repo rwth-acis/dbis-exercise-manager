512a53fcb0b34268f1719422affa6cec8a614456
Wolfgang Fahl <wf@bitplan.com>
2022-07-11 13:14:48 +0200

    changes version matrix from 3.7-3.9 to 3.8-3.10

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     license="Apache",
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10'
