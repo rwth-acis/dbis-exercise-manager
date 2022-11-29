59c0d5106b4b2271317796c342734fc9634e63d8
CodingTil <36734749+CodingTil@users.noreply.github.com>
2022-11-29 15:45:32 +0100

    Python 3.11 Test & Version Bump

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
     ],
     packages=["excmanager"],
     include_package_data=True,
